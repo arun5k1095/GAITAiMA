db7c1e3a54b38637318196c0c103f4a3d6b54b6f
Arun kumar <46933151+arun5k1095@users.noreply.github.com>
2022-05-04 03:42:30 +0200

    Add files via upload

---
+++ FaultCodes.py
@@ -0,0 +1,15 @@
+FaultCodes = {
+100	:	"UserID already exists"	,
+101	:	"UserID Should be entered using only characters and numbers"	,
+102	:	"Name is empty"	,
+103	:	"Name should be entered in characters only"	,
+104	:	"Valid Mail ID eg. abc@gmail.com"	,
+105	:	"Mail Is Empty"	,
+106	:	"Select the Gender"	,
+107	:	"Date of Birth is Not valid"	,
+108	:	"Date of Birth is Not valid"	,
+109	:	"Password should be more than 6 characters"	,
+110	:	"Password should contain 1-UpperUppercase 1-Smallcase 1- special character and 1-digit"	,
+111	:	"User has met all the criteria and ready to register...",
+112 :   "Entered Passwords do not match"
+}
